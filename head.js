fb7d2ff8266bcb87940382b53c2a1c52c93fd4f0
Tai <Tai.Delisle@telus.com>
2022-11-14 14:53:12 -0700

    Add head function

---
+++ head.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('\033[42m', `⭐️ Assertion Passed:`, '\033[0m', `${actual} === ${expected}`);
+  } else {
+    console.log('\033[41m', `🚨 Assertion Failed:`, '\033[0m', `${actual} !== ${expected}`);
+  }
+};
+
+const head = array => array[0];
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), "Hello"); //undefined
+assertEqual(head(["Hello"]), "Hello");
