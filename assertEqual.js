206aefb2320de2d98c0c07404dbe2d38964cf982
Tai <Tai.Delisle@telus.com>
2022-11-14 14:34:06 -0700

    Add assertion function

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('\033[42m', `⭐️ Assertion Passed:`, '\033[0m', `${actual} === ${expected}`);
+  } else {
+    console.log('\033[41m', `🚨 Assertion Failed:`, '\033[0m', `${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Tai", "Tai");
+assertEqual(13, 23);
+assertEqual(23, "23");
