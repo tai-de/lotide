f4ba8dce6eb9733fbbdecffe49deffc74bf84c10
Tai <Tai.Delisle@telus.com>
2022-11-15 10:14:18 -0700

    fix assertArraysEqual and eqArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,16 @@
 const assertArraysEqual = function(arrayOne, arrayTwo) {
-  let result = false;
-
-  if (arrayOne.length === arrayTwo.length) {
-    for (let i = 0; i < arrayOne.length; i++) {
-      if (arrayOne[i] === arrayTwo[i]) {
-        result = true;
-        continue;
-      }
-      result = false;
-      break;
-    }
-  }
-
+  let result = eqArrays(arrayOne, arrayTwo);
   if (result) return console.log('\033[42m', `⭐️ Assertion Passed:`, '\033[0m', `${arrayOne} === ${arrayTwo}`);
   console.log('\033[41m', `🚨 Assertion Failed:`, '\033[0m', `${arrayOne} !== ${arrayTwo}`);
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) return false;
+
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] === arrayTwo[i]) continue;
+    return false;
+  }
+
+  return true;
 };
