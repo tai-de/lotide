323f1dd6b423ec8a56646069214164c32a6a3226
Tai <Tai.Delisle@telus.com>
2022-11-16 09:26:53 -0700

    initial commit for eqObjects function, test codes written, no evaluations yet

---
+++ eqObjects.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('\033[42m', `### Assertion Passed ###`, '\t\033[0m', `${actual} \t === \t ${expected}`);
+  } else {
+    console.log('\033[41m', `### Assertion Failed ###`, '\t\033[0m', `${actual} \t !== \t ${expected}`);
+  }
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
+
+assertEqual(eqObjects(ab,ba),true)
+assertEqual(eqObjects(ab,abc),false)
