007a188e5ffec1d3e4f32f62cf13526e5817aacf
Tai <Tai.Delisle@telus.com>
2022-11-16 09:11:41 -0700

    update with for in loop tested, confirmed to return values on each loop

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -7,7 +7,12 @@ const assertEqual = function(actual, expected) {
 };
 
 const findKeyByValue = function(object, value) {
-
+  for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+      const element = object[key];
+      console.log(element);
+    }
+  }
   return undefined;
 };
 
