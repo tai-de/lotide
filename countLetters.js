bf726e810b3b685992ddef33f0d6aa3e3ffce836
Tai <Tai.Delisle@telus.com>
2022-11-15 23:05:12 -0700

    update countLetters functionality to ignore spaces

--- countLetters.js
+++ countLetters.js
@@ -7,11 +7,14 @@ const assertEqual = function(actual, expected) {
 };
 
 const countLetters = function(string) {
+  const trimmedString = string.replaceAll(" ", "");
   const output = {};
-  for (const letter of string) {
+
+  for (const letter of trimmedString) {
     let value = letter.toLowerCase();
     !output[value] ? output[value] = 1 : output[value]++;
   }
+
   return output;
 };
 
