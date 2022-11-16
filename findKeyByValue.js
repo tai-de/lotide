9add31d9e4fc988a41e5077c5e02647515e23dbf
Tai <Tai.Delisle@telus.com>
2022-11-16 09:09:08 -0700

    update findKeyByValue with test cases, no functionality yet

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,15 +1,26 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) return false;
-
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] === arrayTwo[i]) continue;
-    return false;
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('\033[42m', `### Assertion Passed ###`, '\t\033[0m', `${actual} \t === \t ${expected}`);
+  } else {
+    console.log('\033[41m', `### Assertion Failed ###`, '\t\033[0m', `${actual} \t !== \t ${expected}`);
   }
-
-  return true;
 };
 
 const findKeyByValue = function(object, value) {
 
-
+  return undefined;
 };
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+  space: "The Expanse"
+}
+
+// console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine")); // comedy
+// console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse")); // sci_fi
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"),"comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"),"sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Scandal"),undefined);
