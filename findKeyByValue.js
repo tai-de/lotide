f5a936dfc56de1450ab677a7bbe52c0568f49f6c
Tai <Tai.Delisle@telus.com>
2022-11-16 09:15:11 -0700

    update working github version, commented out test cases

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -16,16 +16,13 @@ const findKeyByValue = function(object, value) {
   return undefined;
 };
 
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire",
-  space: "The Expanse"
-}
+// const bestTVShowsByGenre = {
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama: "The Wire",
+//   space: "The Expanse"
+// }
 
-// console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine")); // comedy
-// console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse")); // sci_fi
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"),"comedy");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"),"sci_fi");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Scandal"),undefined);
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"),"comedy");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"),"sci_fi");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "Scandal"),undefined);
