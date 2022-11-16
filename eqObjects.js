76c38c9e939d9083b06ca0d3f3334e84e62bab30
Tai <Tai.Delisle@telus.com>
2022-11-16 09:33:16 -0700

    update with working test cases for primitive values

--- eqObjects.js
+++ eqObjects.js
@@ -9,6 +9,16 @@ const assertEqual = function(actual, expected) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+  if (object1Keys.length !== object2Keys.length) return false; // stop function if different key quantity
+
+  for (const key in object1Keys) {
+    if (object1[key] !== object2[key]) return false; // stop once different key is found
+    continue;
+  }
+
+  return true;
 };
 
 const ab = { a: "1", b: "2" };
