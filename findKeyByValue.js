a4fe1d56369f1f7a04c625db0f4aa5d3dd186838
Tai <Tai.Delisle@telus.com>
2022-11-16 09:14:33 -0700

    update after successful test cases for findKeyByValue function

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -10,7 +10,7 @@ const findKeyByValue = function(object, value) {
   for (const key in object) {
     if (Object.hasOwnProperty.call(object, key)) {
       const element = object[key];
-      console.log(element);
+      if(element === value) return key;
     }
   }
   return undefined;
