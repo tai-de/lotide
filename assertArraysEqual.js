f88cdaa16cdcfcd69972b2a0c26a7acff1a440d9
Tai <Tai.Delisle@telus.com>
2022-11-21 08:42:59 -0700

    refactor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,9 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(actual, expected) {
   let result = eqArrays(actual, expected);
   if (result) return console.log('\033[42m', `### Assertion Passed ###`, '\t\033[0m', `${actual} \t === \t ${expected}`);
   console.log('\033[41m', `### Assertion Failed ###`, '\t\033[0m', `${actual} \t !== \t ${expected}`);
 };
+
+module.exports = assertArraysEqual;
