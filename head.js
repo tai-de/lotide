43aff6db9646c0599a3154c307771f09f19cbbcf
Tai <Tai.Delisle@telus.com>
2022-11-21 08:37:51 -0700

    refactor tail

--- head.js
+++ head.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual');
-
 const head = array => array[0];
 
 module.exports = head;
--- tail.js
+++ tail.js
@@ -1,17 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log('\033[42m', `### Assertion Passed ###`, '\t\033[0m', `${actual} \t === \t ${expected}`);
-  } else {
-    console.log('\033[41m', `### Assertion Failed ###`, '\t\033[0m', `${actual} \t !== \t ${expected}`);
-  }
-};
-
 const tail = function(array) {
   let tailArray = array.slice(1);
   return tailArray;
 };
 
-// Test Case: Check the original array
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
